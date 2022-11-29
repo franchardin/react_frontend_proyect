98489a22e340c0fb71b303313d9cd2b9fd327b5f
franchardin <francisco.chardin@gmail.com>
2022-11-29 10:40:07 -0300

    detalles visuales en login y select de texto celeste en app

--- team3-frontendproyect/src/pages/Login/Login.js
+++ team3-frontendproyect/src/pages/Login/Login.js
@@ -14,14 +14,14 @@ const Login = () => {
                 <div className="showcase-content">
                     <><div className="formm">
                         <form>
-                            <h1>Log In</h1>
+                            <h1>Login</h1>
                             <div className="info">
                                 <input className="email" type="email" placeholder="Email or phone number"></input>
                                 <br />
                                 <input className="email" type="password" placeholder="Password"></input>
                             </div>
                             <div className="btn">
-                                <button className="btn-primary" type="submit">Log In</button>
+                                <button className="btn btn-primary" type="submit">Login</button>
                             </div>
                             <div className="remember">
                                 <div>
@@ -32,7 +32,6 @@ const Login = () => {
                     </div>
                         <div className="signup">
                             <p>New to Netflix?</p>
-
                             <p><Link to="Signup">Signup</Link></p>
                             <Routes>
                                 <Route path='Signup' element={<Signup />} />
